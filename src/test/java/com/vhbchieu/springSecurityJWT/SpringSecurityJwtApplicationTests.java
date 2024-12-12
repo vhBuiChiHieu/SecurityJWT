2f2c2bd1dc363255efb8cf3b4a997372794c5ad0
vhBuiChiHieu <vh.bchieu@gmail.com>
2024-12-12 14:22:15 +0700

    first commit

---
+++ src/test/java/com/vhbchieu/springSecurityJWT/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhbchieu.springSecurityJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
